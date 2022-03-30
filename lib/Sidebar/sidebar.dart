63246d0fc32cb968fb05d3ca4ff378dfa8d86f32
Sameep <sameepsawant10@gmail.com>
2022-03-30 12:23:39 +0530

    side bar creatd

--- lib/Sidebar/sidebar.dart
+++ lib/Sidebar/sidebar.dart
@@ -1,22 +1,33 @@
 import "package:flutter/material.dart";
 
-class SideBar extends StatelessWidget {
+
+class SideBar extends StatefulWidget{
+  @override
+  _SideBarState createState() => _SideBarState();
+}
+
+class _SideBarState extends State<SideBar> {
+  final bool isSideBarOpened = false;
+
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      Expanded(
-        child: Container(
-          color: Colors.red,
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    return Positioned(
+      top:0,
+      bottom:0,
+      left: isSideBarOpened ? 0 :0,
+      right: isSideBarOpened ? 0 : screenWidth - 45,
+      child: Row(children: <Widget>[
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
         ),
-      ),
-      Align(
-      alignment: Alignment(0,-0.9),
-      child: Container(
-        width: 35,
-        height: 110,
-        color:Colors.red
-      )
-      )
-    ]);
+        Align(
+            alignment: Alignment(0, -0.9),
+            child: Container(width: 35, height: 110, color: Colors.red))
+      ]),
+    );
   }
 }
